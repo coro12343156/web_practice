2899a43621843537d7d03cbcfe76b13edbaeb375
taltal1127 <4958sh19@gmail.com>
2026-02-18 10:15:34 +0000

    test

---
+++ student/main.py
@@ -0,0 +1,38 @@
+a = True
+b = False
+
+if a == b:
+  print("false")
+else:
+   print("true")
+
+if a and b:
+   print("2keta")
+else:
+   print("1keta")
+
+x =3
+
+if 1<=x and x<=4:
+ print("nyako")
+
+if 0>= x or 10<= x:
+  print("nyako") 
+
+a= True
+b= False
+  if a =True:
+    print("1")
+  elif b=1:
+    print("0")
+ else:
+   print("1")
+
+if a==True or b == False:
+  print("1")
+else:
+  print("0")
+  
+
+
+
