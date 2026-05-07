e862d4f1fcc4db450696f6f9f540d76aa5d2baeb
coro <95575239+coro12343156@users.noreply.github.com>
2026-05-07 11:31:25 +0000

    辞書説明ファイル

---
+++ teacher/test.py
@@ -0,0 +1,26 @@
+hantai = {
+    27 : { 
+        1: {
+            "kaesu":[28],
+            "sitei":29
+        },
+        -1: {
+            "kaesu":[26, 25, 24],
+            "sitei":23
+        }
+    },
+    36 : {
+        ...
+    }
+}
+
+
+
+def func():
+    aaa = 0
+    global bbb
+    bbb = 1
+
+print(aaa)
+
+bbb = 0
